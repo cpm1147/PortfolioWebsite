59a0f6004eaeee61c23263175a819d2e8c6d64c3
cpm1147 <cpm1147@autuni.ac.nz>
2026-03-11 20:59:42 +1300

    project initialisation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Projects } from './components/projects/projects';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Projects],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('PortfolioWebsite');
+  name: string = "Willie";
 }
---
+++ src/app/components/projects/projects.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  imports: [],
+  templateUrl: './projects.html',
+  styleUrl: './projects.css',
+})
+export class Projects {
+  title: string = "My Projects";
+  goBearAiLink: string = "future-forge-project.vercel.app";
+
+  
+}
