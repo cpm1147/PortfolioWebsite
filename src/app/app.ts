5a0de1eeb233a532a717701caac1b5aafd12be92
cpm1147 <cpm1147@autuni.ac.nz>
2026-03-11 21:49:27 +1300

    added navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Projects } from './projects/projects';
 import { Header } from './header/header';
+import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Projects],
+  imports: [RouterOutlet, Header, Navbar, Projects],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
