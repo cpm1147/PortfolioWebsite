ca077d6043b5c57c03f4e2dafd95c88e75c2a788
cpm1147 <cpm1147@autuni.ac.nz>
2026-03-12 16:57:58 +1300

    updated home page and added project cards

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ProjectCard } from '../project-card/project-card';
+
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [ProjectCard],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
