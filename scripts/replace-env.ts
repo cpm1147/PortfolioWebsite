720d766f03bee431dad715707af91ea28c6c15f6
cpm1147 <cpm1147@autuni.ac.nz>
2026-03-31 21:58:04 +1300

    fix env script

--- scripts/replace-env.ts
+++ scripts/replace-env.ts
@@ -7,10 +7,10 @@ const targetPath = path.join(__dirname, '../src/environments/environment.prod.ts
 
 const envConfigFile = `export const environment = {
   production: true,
-  emailJsServiceId: '${process.env.EMAILJS_SERVICE_ID || ''}',
-  emailJsTemplateId: '${process.env.EMAILJS_TEMPLATE_ID || ''}',
-  emailJsPublicKey: '${process.env.EMAILJS_PUBLIC_KEY || ''}',
-  recaptchaSiteKey: '${process.env.RECAPTCHA_SITE_KEY || ''}'
+  emailJsServiceId: '${process.env['EMAILJS_SERVICE_ID'] || ''}',
+  emailJsTemplateId: '${process.env['EMAILJS_TEMPLATE_ID'] || ''}',
+  emailJsPublicKey: '${process.env['EMAILJS_PUBLIC_KEY'] || ''}',
+  recaptchaSiteKey: '${process.env['RECAPTCHA_SITE_KEY'] || ''}'
 };
 `;
 
