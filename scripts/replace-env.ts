0288520b5cd3537efbff791227b4021ac2c3235c
cpm1147 <cpm1147@autuni.ac.nz>
2026-03-31 21:30:55 +1300

    setting up for vercel deployment

---
+++ scripts/replace-env.ts
@@ -0,0 +1,18 @@
+/// <reference types="node" />
+
+import fs from 'fs';
+import path from 'path';
+
+const targetPath = path.join(__dirname, '../src/environments/environment.prod.ts');
+
+const envConfigFile = `export const environment = {
+  production: true,
+  emailJsServiceId: '${process.env.EMAILJS_SERVICE_ID || ''}',
+  emailJsTemplateId: '${process.env.EMAILJS_TEMPLATE_ID || ''}',
+  emailJsPublicKey: '${process.env.EMAILJS_PUBLIC_KEY || ''}',
+  recaptchaSiteKey: '${process.env.RECAPTCHA_SITE_KEY || ''}'
+};
+`;
+
+fs.writeFileSync(targetPath, envConfigFile);
+console.log('Environment.prod.ts updated.');
