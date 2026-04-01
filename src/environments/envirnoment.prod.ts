de5c30087a600e1c637406ad7b20a7f5cc24d603
cpm1147 <cpm1147@autuni.ac.nz>
2026-04-01 18:42:22 +1300

    forgot to save environment.prod

--- src/environments/envirnoment.prod.ts
+++ src/environments/envirnoment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  emailJsServiceId: '',
-  emailJsTemplateId: '',
-  emailJsPublicKey: '',
-  recaptchaSiteKey: ''
+  emailJsServiceId: (process.env['EMAILJS_SERVICE_ID'] as string) || '',
+  emailJsTemplateId: (process.env['EMAILJS_TEMPLATE_ID'] as string) || '',
+  emailJsPublicKey: (process.env['EMAILJS_PUBLIC_KEY'] as string) || '',
+  recaptchaSiteKey: (process.env['RECAPTCHA_SITE_KEY'] as string) || ''
 };
