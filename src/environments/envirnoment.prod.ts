068036a64a0b3a6402010f7e00850f7f4e508ade
cpm1147 <cpm1147@autuni.ac.nz>
2026-04-01 19:09:20 +1300

    env not working so hard coding

--- src/environments/envirnoment.prod.ts
+++ src/environments/envirnoment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  emailJsServiceId: (process.env['EMAILJS_SERVICE_ID'] as string) || '',
-  emailJsTemplateId: (process.env['EMAILJS_TEMPLATE_ID'] as string) || '',
-  emailJsPublicKey: (process.env['EMAILJS_PUBLIC_KEY'] as string) || '',
-  recaptchaSiteKey: (process.env['RECAPTCHA_SITE_KEY'] as string) || ''
+  emailJsServiceId: 'service_h79w7fv',
+  emailJsTemplateId: 'template_zzy6v0d',
+  emailJsPublicKey: '3rGr33ICJb5O2xFkv',
+  recaptchaSiteKey: '6Lckw58sAAAAAHILogmhnKvxpaj4dWr8IpIe_P4Q'
 };
